8ccf7413b963361d0ee0b9de3fb9de47cd5d452b
Steinar Bang <sb@dod.no>
2025-03-02 11:34:56 +0100

    Use runtime incompatible version 2.0.0 of FrontendServlet that reads routes from assets/routes.txt in classpath

--- handlereg.web.frontend/src/main/java/no/priv/bang/handlereg/web/frontend/HandleregServlet.java
+++ handlereg.web.frontend/src/main/java/no/priv/bang/handlereg/web/frontend/HandleregServlet.java
@@ -21,15 +21,11 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import static org.osgi.service.http.whiteboard.HttpWhiteboardConstants.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardContextSelect;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletName;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletPattern;
 import org.osgi.service.log.LogService;
 
-import no.priv.bang.handlereg.services.HandleregException;
 import no.priv.bang.servlet.frontend.FrontendServlet;
 
 @Component(service=Servlet.class, immediate=true)
@@ -40,23 +36,12 @@ public class HandleregServlet extends FrontendServlet {
     private static final long serialVersionUID = -3496606785818930881L;
 
     public HandleregServlet() {
-        super();
-        // The paths used by the react router
-        setRoutes(readLinesFromClasspath("assets/routes.txt"));
+        super(HandleregServlet.class);
     }
 
     @Override
     @Reference
     public void setLogService(LogService logservice) {
         super.setLogService(logservice);
-    }
-
-    String[] readLinesFromClasspath(String fileName) {
-        try (var reader = new BufferedReader(new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream(fileName)))) {
-            var lines = reader.lines().toList();
-            return lines.toArray(new String[0]);
-        } catch (Exception e) {
-            throw new HandleregException("Failed to read routes list from classpath resource", e);
-        }
     }
 }
