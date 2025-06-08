879b38814da4ea3a73a4b0421024ac58f687421e
Steinar Bang <sb@dod.no>
2025-06-08 08:43:08 +0200

    Revert "Bump tailwindcss in /handlereg.web.frontend/src/main/frontend"
    New tailwind didn't work and I don't wish to spend time
    troubleshooting it.  Better to remove tailwind altogether.

--- handlereg.web.frontend/src/main/frontend/postcss.config.cjs
+++ handlereg.web.frontend/src/main/frontend/postcss.config.cjs
@@ -1,5 +1,6 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  }
+  plugins: [tailwindcss('./tailwind.config.cjs'), autoprefixer],
 };
