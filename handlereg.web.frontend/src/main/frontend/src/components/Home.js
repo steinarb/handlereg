c4a5b31fd1395008c674ca0a01c6346111c54cf3
Steinar Bang <sb@dod.no>
2025-07-17 23:59:13 +0200

    Change order of buttons to make up button always be rightmost in top nav bar

--- handlereg.web.frontend/src/main/frontend/src/components/Home.js
+++ handlereg.web.frontend/src/main/frontend/src/components/Home.js
@@ -45,8 +45,8 @@ export default function Home() {
             <div className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
                 <a className="pure-menu-heading">Matregnskap</a>
                 <ul className="pure-menu-list">
-                    <li className="pure-menu-item"><a className="pure-menu-link" href="../..">Opp</a></li>
                     <li className="pure-menu-item"><Link className="pure-menu-link" to="/hurtigregistrering">Hurtig</Link></li>
+                    <li className="pure-menu-item"><a className="pure-menu-link" href="../..">Opp</a></li>
                 </ul>
             </div>
             <div className="content-wrapper navigation-link-list">
