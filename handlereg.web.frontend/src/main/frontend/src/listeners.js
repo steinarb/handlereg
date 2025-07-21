6e4fb442decdfa55f50722d0705daf6793342b9e
Steinar Bang <sb@dod.no>
2025-07-21 08:49:56 +0200

    Use RTK listener to select store of last transaction as current store

--- handlereg.web.frontend/src/main/frontend/src/listeners.js
+++ handlereg.web.frontend/src/main/frontend/src/listeners.js
@@ -2,7 +2,7 @@ import { createListenerMiddleware } from '@reduxjs/toolkit';
 import { isAnyOf } from '@reduxjs/toolkit';
 import { api } from './api';
 import { VIS_KVITTERING, LOCATION_CHANGE } from './actiontypes';
-import { blankUtButikk } from './reducers/butikkSlice';
+import { blankUtButikk, velgButikk } from './reducers/butikkSlice';
 
 const listenerMiddleware = createListenerMiddleware();
 
@@ -53,6 +53,20 @@ listenerMiddleware.startListening({
         const newLocation = action.payload.location.pathname;
         if (basename + '/nybutikk' === newLocation) {
             listenerApi.dispatch(blankUtButikk()); // Blank store name form when navigating into new store component
+        }
+    }
+})
+
+// Select store of last transaction as the current store
+listenerMiddleware.startListening({
+    matcher: api.endpoints.getHandlinger.matchFulfilled,
+    effect: (action, listenerApi) => {
+        // Find first element of first page of inifinite query, and ignore the rest
+        if (action.payload.pageParams.length === 1 && action.payload.pageParams[0] === 0) {
+            const firstTransaction = action.payload.pages[0][0];
+            const butikker = listenerApi.getState().api.queries['getButikker(undefined)'].data;
+            const sistBrukteButikk = butikker.find(b => b.storeId === firstTransaction.storeId);
+            listenerApi.dispatch(velgButikk(sistBrukteButikk));
         }
     }
 })
